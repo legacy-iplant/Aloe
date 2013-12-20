accf77e476bd95b77011bb5ed758051b8d31d7c2
Dustin Landers <dustin.landers@gmail.com>
2013-12-20 09:15:41 -0500

    added multiple select columns and other

--- aloe.py
+++ aloe.py
@@ -97,36 +97,47 @@ class Application(Frame):
             self.listBox.delete(0, END)
         for item in currentList:
             self.listBox.insert(END, item)
-        print currentList
+        #print currentList
 
-    def printthing(self):
+    def select(self):
         global currentList
-        print map(int, self.listBox.curselection())
+        namesToKeep = list()
+        for each in map(int, self.listBox.curselection()):
+            namesToKeep.append(currentList[each])
+        self.listBox.delete(0, END)
+        currentList = namesToKeep
+        for item in currentList:
+            self.listBox.insert(END, item)
+        #print currentList
+
+    def printCurrentList(self):
+        global currentList
+        print currentList
 
     def createWidgets(self):
         self.logo = PhotoImage(file="logo.gif")
         self.Title = Label(self, image=self.logo)
-        self.Title.grid(row=0, column=0, columnspan=3, pady=10)
+        self.Title.grid(row=0, column=0, columnspan=6, pady=10)
 
         self.Submit = Button(self)
         self.Submit["text"] = "Submit"
         self.Submit["command"] = self.submitRequest
-        self.Submit.grid(row=3, column=2, pady=10)
+        self.Submit.grid(row=3, column=5)
 
         self.QUIT = Button(self)
         self.QUIT["text"] = "Quit"
         self.QUIT["command"] = self.quit
-        self.QUIT.grid(row=4, column=2, pady=10)
+        self.QUIT.grid(row=4, column=5)
 
         self.action = StringVar(self)
         self.action.set("get")
         self.actionOption = OptionMenu(self, self.action, "get", "post", "put", "delete")
-        self.actionOption.grid(row=1, column=1, pady=10)
+        self.actionOption.grid(row=1, column=2, columnspan=2, pady=7.5)
 
         self.rtypestr = StringVar(self)
         self.rtypestr.set("files")
         self.rtypeOption = OptionMenu(self, self.rtypestr, "files", "apps")
-        self.rtypeOption.grid(row=2, column=1, pady=10)
+        self.rtypeOption.grid(row=2, column=2, columnspan=2, pady=7.5)
 
         self.label1 = Label(self)
         self.label1["text"] = "Request"
@@ -139,7 +150,7 @@ class Application(Frame):
         self.specifics = StringVar(self)
         self.specifics.set("listings/")
         self.specificsEntry = Entry(self, textvariable=self.specifics)
-        self.specificsEntry.grid(row=3, column=1, pady=10, padx=30)
+        self.specificsEntry.grid(row=3, column=2, columnspan=2, pady=7.5)
 
         self.label3 = Label(self)
         self.label3["text"] = "Location"
@@ -148,29 +159,50 @@ class Application(Frame):
         self.resultType = StringVar(self)
         self.resultType.set("name")
         self.resultTypeEntry = Entry(self, textvariable=self.resultType)
-        self.resultTypeEntry.grid(row=4, column=1, pady=10, padx=30)
+        self.resultTypeEntry.grid(row=4, column=2, columnspan=2, pady=7.5)
 
         self.label4 = Label(self)
         self.label4["text"] = "Result Type"
         self.label4.grid(row=4, column=0)
 
         self.label5 = Label(self, fg='red')
-        self.label5["text"] = "Selected List"
-        self.label5.grid(row=5, column=0, pady=20)
+        self.label5["text"] = "Selected List A"
+        self.label5.grid(row=6, column=0)
 
         self.listBox = Listbox(self, selectmode=EXTENDED)
-        self.listBox.config(height=30, width=100)
-        self.listBox.grid(row=6, column=0, columnspan=3)
+        self.listBox.config(height=30, width=50)
+        self.listBox.grid(row=7, column=0, columnspan=3)
 
-        #self.clear = Button(self)
-        #self.clear["text"] = "Clear"
-        #self.clear["command"] = self.clearListBox
-        #self.clear.grid(row=1, column=2)
+        self.label7 = Label(self, fg='red')
+        self.label7["text"] = "Selected List B"
+        self.label7.grid(row=6, column=3)
+
+        self.listBoxB = Listbox(self, selectmode=EXTENDED)
+        self.listBoxB.config(height=30, width=50)
+        self.listBoxB.grid(row=7, column=3, columnspan=3)
 
         self.listDelete = Button(self)
         self.listDelete["text"] = "Delete"
         self.listDelete["command"] = self.deleteListBoxSelect
-        self.listDelete.grid(row=2, column=2)
+        self.listDelete.grid(row=2, column=5)
+
+        self.selectMe = Button(self)
+        self.selectMe["text"] = "Select"
+        self.selectMe["command"] = self.select
+        self.selectMe.grid(row=1, column=5)
+
+        self.printCL = Button(self)
+        self.printCL["text"] = "Print Selection"
+        self.printCL["command"] = self.printCurrentList
+        self.printCL.grid(row=0, column=5)
+
+        self.label6 = Label(self)
+        self.label6["text"] = "Data"
+        self.label6.grid(row=5, column=0)
+
+        self.data = StringVar()
+        self.dataEntry = Entry(self, textvariable=self.data)
+        self.dataEntry.grid(row=5, column=2, columnspan=2, pady=7.5)
 
 root = Tk()
 root.wm_title('Aloe for Agave')
